25363436464b0e76d4053878ac0725e8ba3aa56a
Hunter Browning <guccigod@Hunters-MacBook-Pro.local>
2022-02-02 10:47:01 -0500

    Added Variables

---
+++ 01_Variables/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello! What is your name?");
+
+string firstName = Console.ReadLine();
+Console.WriteLine("Hello, " + firstName);
+
+Console.WriteLine("What is your favorite color?");
+string color = Console.ReadLine();
+
+Console.WriteLine("You said your favorite color is " + color);
