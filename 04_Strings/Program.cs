fc643c283bc393751db58c454c15d1598d22cdc8
Hunter Browning <guccigod@Hunters-MacBook-Pro.local>
2022-02-02 12:24:39 -0500

    Adding 04_Strings to Fundamentals Solution/Project

---
+++ 04_Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿string first = "The cars we sell are";
+string second = "BMW, Lexus, Mercedes and Tesla.";
+
+string concatenatedResult = first + " " + second;
+
+Console.WriteLine(concatenatedResult);
+
+string firstName = "Jenn";
+string lastName = "Williams";
+
+string compositeResult = string.Format("Her name is {0} {1}", firstName, lastName);
+
+Console.WriteLine(compositeResult);
+Console.WriteLine("Her name is {0} {1}", firstName, lastName);
+
+string interpolationResult = $"Her name is still {firstName} {lastName}!";
+Console.WriteLine(interpolationResult);
