3f44142f185be2dfaee18092ba474bb3e7ca249b
Hunter Browning <guccigod@Hunters-MacBook-Pro.local>
2022-02-04 09:33:30 -0500

    More Conditionals added to solution/project

--- 06_Conditionals/Switches/Program.cs
+++ 06_Conditionals/Switches/Program.cs
@@ -1,8 +1,21 @@
 ﻿Console.WriteLine("Please give us your name!");
 string name = Console.ReadLine();
+
+string greeting = name switch
+{
+    "Hunter" => "Hello Hunter",
+    _ => "Who are you?"
+
+};
+Console.WriteLine(greeting);
+
 switch (name)
 {
-    case "Hunter";
+    case "Hunter":
+        Console.WriteLine("Nice to meet you " + name);
+        break;
+    default:
+        break;
 }
 
 Console.WriteLine("How are you feeling today on a scale of 1-5?");
@@ -28,5 +41,22 @@ switch (feelingRating)
     default:
         Console.WriteLine("Sorry, wht you put in is not what we asked for.");
         break;
-
 }
+
+
+Console.WriteLine("How were you feeling yesterday on a scale of 1-5?");
+String ratingInput = Console.ReadLine();
+int yesterdayRating = int.Parse(ratingInput);
+
+string response = yesterdayRating switch
+{
+    1 => "Dang. Maybe today will be better",
+    2 => "Oh, sorry to hear that.",
+    3 => "Nice, sounds like a good day",
+    4 => "Good stuff",
+    5 => "That is wonderful to hear",
+    _ => "Man you are off the charts!"
+
+};
+
+Console.WriteLine(response);
---
+++ 06_Conditionals/Ternaries/Program.cs
@@ -0,0 +1,14 @@
+﻿// Here we're using Write instead of WriteLine
+Console.Write("Enter your age: ");
+string response = System.Console.ReadLine();
+int age = int.Parse(response);
+
+//1             //2        //3               //4
+String output = age >= 18 ? "You can vote!" : "You're too young to vote.";
+
+// 1 - Variable to hold returned value
+// 2 - Boolean expression to be evaluated
+// 3 - Value to be returned if 2 is true
+// 4 - Value to be returned if 2 is false
+
+Console.WriteLine(output);
